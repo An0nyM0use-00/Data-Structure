95a00025204e091be0c6d407d34ddb5ae0ce13b4
EJ Ricalde <jamesricalde13@gmail.com>
2025-08-13 16:28:50 +0800

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,20 +1,19 @@
 numbers = []
-print("Enter numbers one by one. Type 'done' to finish.")
+print("Enter how many numbers you want to input:")
+count = int(input("Number of inputs : "))
 
-while True:
-    user_input = input("Enter a number (or 'done' to exit): ")
-
-    if user_input.lower() == 'done':
-        break
-    
+print("Enter numbers one by one.")
+for _ in range(count):
+    user_input = input("Enter a number : ")
     numbers.append(int(user_input))
-    
-    n = len(numbers)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if numbers[j] > numbers[j+1]:
-                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
-    
-    print("Current sorted list (Bubble Sorted):", numbers)
 
-print("Final sorted list (Bubble Sorted):", numbers)
+n=len(numbers)
+for i in range(n):
+    for j in range(0, n-i-1):
+        if numbers[j]> numbers[j+1]:
+            numbers[j], numbers[j+1] = numbers[j+ 1], numbers[j]
+
+display_choice = input("How would you like to display the final sorted list? (type 'asc' for ascending or 'desc' for descending):")
+if display_choice.lower() == 'desc':
+    numbers.reverse()
+print("Final sorted list :", numbers)
